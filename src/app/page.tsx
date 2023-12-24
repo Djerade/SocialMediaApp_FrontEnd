60fc6606dc489748ab61efd6aa3f6198f5161b29
Djeradé Golbé Parfait <djeradegolbeparfait@gmail.com>
2023-12-24 04:38:24 +0000

    init and config

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import { Link } from '@chakra-ui/next-js'
+
+export default function Home() {
+  return (
+    <Link href='/about' color='blue.400' _hover={{ color: 'blue.500' }}>
+      About
+    </Link>
+  )
+}
