411e86e69a4a4f9409e437ac5f805b36ed24d2fd
Djeradé Golbé Parfait <djeradegolbeparfait@gmail.com>
2023-12-26 08:22:30 +0000

    login ui

--- src/app/UI/auth/index.tsx
+++ src/app/UI/auth/index.tsx
@@ -1,35 +1,44 @@
 import LogoName from '@/app/components/logoName/logoName'
-import { Button, Container, Text, Box, Flex, FormControl, FormLabel, Input, Divider, AbsoluteCenter } from '@chakra-ui/react'
+import { Button, Text, Box, Flex, FormControl, Input, Divider, AbsoluteCenter } from '@chakra-ui/react'
 import { NextPage } from 'next'
 
 
 interface Props {}
 
 const Login: NextPage<Props> = ({}) => {
-    return <Flex align={'center'} justify={'center'} w={{ base: "", sm: "", md: "", lg:""}}>
-        <Flex  align={'center'} mt={10} flexDirection={'column'} w={{ base: '20%', sm: '20%', md: "20%", lg: "30%" }}>
-            <LogoName/>
-            <FormControl mt={4}>
-                <Input placeholder={"Nom d'utilisateur"} />
-            </FormControl>
-            <FormControl mt={4}>
-                <Input placeholder={"Mot de passe"} />
-            </FormControl>
-            <Box
-                position={'relative'}
-                p={10}
-            >
-                <Divider/>
-                <AbsoluteCenter>
-                    ou
-                </AbsoluteCenter>
-            </Box>
-            <Button
-                type='submit'
-                mt={4}
-            >
-                Se connecter
-            </Button>
+    return <Flex align={'center'} justify={'center'} w={{ base: "", sm: "", md: "", lg: "" }}>
+        <Flex align={'center'} w={"100%"} flexDirection={'column'}>
+            <Flex boxShadow={'base'} p={8} align={'center'} mt={10} flexDirection={'column'} w={{ base: '90%', sm: '60%', md: "40%", lg: "30%" }}>
+                <LogoName/>
+                <FormControl mt={4}>
+                    <Input placeholder={"Nom d'utilisateur"} />
+                </FormControl>
+                <FormControl mt={4}>
+                    <Input placeholder={"Mot de passe"} />
+                </FormControl>
+                <Box
+                    position={'relative'}
+                    p={10}
+                >
+                    <Divider/>
+                    <AbsoluteCenter bg={'white'} px={4}>
+                        ou
+                    </AbsoluteCenter>
+                </Box>
+                <Button
+                    type='submit'
+                    mt={4}
+                    w={'100%'}
+                    color={'white'}
+                    bg={'blue'}
+                >
+                    Se connecter
+                </Button>
+            </Flex>
+            <Flex boxShadow={'base'} p={8} align={'center'} mt={10} flexDirection={'row'} w={{ base: '90%', sm: '60%', md: "40%", lg: "30%" }}>
+                <Text variant=''>Vous avez pas de compte ? </Text>
+                <Text color={'blue'} variant=''>Inscrivez vous </Text>
+            </Flex>
         </Flex>
   </Flex>
 }
