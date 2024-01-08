51bc557bdffabac5a838a7af47d32a19f5435b0a
Djeradé Golbé Parfait <djeradegolbeparfait@gmail.com>
2024-01-08 04:32:01 +0000

    update

--- src/app/components/sidebare/index.tsx
+++ src/app/components/sidebare/index.tsx
@@ -1,10 +1,30 @@
-import { Center, Flex, Text } from "@chakra-ui/react"
+'use client'
+import { Box, Center, Flex, Text } from "@chakra-ui/react"
 import LogoImage from "../logoName/logo"
 import Profile from "../profile/index"
 import LogoName from "../logoName/logoName"
 
+interface Item {
+  name: String;
+  value: String;
+}
 const Sidebar = () => {
-    return <Flex
+  const items = [
+    {
+      "name": "Poste",
+      "value": "5842",
+    },
+    {
+      "name": "Followings",
+      "value": "5842",
+    },
+    {
+      "name": "Followers",
+      "value": "5842",
+    }
+  ];
+
+ return <Flex
         w='250px'
         h='100vh'
         display={{ base:"none", sm:"block" }}
@@ -22,12 +42,26 @@ const Sidebar = () => {
         <Profile />
         <Flex mt={'2'} justify={'center'} align={'center'} flexDirection={"column"}>
           <Text fontFamily={"Segoe UI"} fontSize={"lg"} fontWeight={'bold'} color={'black'} >
-            Vera Cherry
+            Vera Cheery
           </Text>
           <Text fontFamily={"Segoe UI"} fontSize={'xs'} fontWeight={'normal'} color={'gray'} >
-            Vera Cherry
+            @cheery
           </Text>
-        </Flex>
+     </Flex>
+     <Flex pr={5} pl={5}  w={'100%'} justify={'space-between'} align={'center'} >
+       {
+        items.map((item: Item) => (
+          <Flex  flexDirection={'column'}  alignItems={'center'}   >
+            <Text fontFamily={"Segoe UI"} fontSize={'sm'} fontWeight={'semibold'} color={'Black'} >
+             {item.value}
+            </Text>
+            <Text fontFamily={"Segoe UI"} fontSize={'xs'} fontWeight={''} color={'gray'} >
+             {item.name}
+            </Text>
+          </Flex>
+         ))
+       }
+     </Flex>
       </Center>
   </Flex>
 }
