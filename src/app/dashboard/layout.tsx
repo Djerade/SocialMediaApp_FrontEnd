5d9c78af13ba3d3cbb696261b847e5080f625176
Djeradé Golbé Parfait <djeradegolbeparfait@gmail.com>
2024-08-08 13:52:47 +0000

    add multiple layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,13 +4,15 @@ import { Box, Flex } from '@chakra-ui/react'
 import { ApolloProvider } from "@apollo/client";
 
 
-import { fonts } from './fonts'
-import { Providers } from './providers'
 
-import theme from './theme'
+
+
 import NaveBar from '@/components/navebar';
 import Sidebar from '@/components/sidebare';
 import client from '@/lib/client';
+import theme from '../theme';
+import { Providers } from '../providers';
+import { fonts } from '../fonts';
 
 
 
@@ -22,19 +24,19 @@ export default function RootLayout({
   children: React.ReactNode,
   }) {
   
-  // const { colorMode } = useColorMode();
-
-
-  
+  // const { colorMode } = useColorMode();  
   return (
     <html lang='en' className={fonts.rubik.variable}>
       <body>
-        <ColorModeScript initialColorMode={theme.initialColorMode} />
-          <ApolloProvider client={client}>
-          <Providers>
+          <Flex  w={'100%'} flexDirection={"row"}>
+            <Sidebar  />
+            <Flex width={'100%'} flexDirection={'column'}>
+              <NaveBar/> 
+              <Box>
                 {children}
-          </Providers>
-          </ApolloProvider>
+              </Box>
+            </Flex>
+          </Flex>
       </body>
     </html>
   )
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,50 @@
+'use client'
+import { useColorMode } from '@chakra-ui/react';
+import { Flex } from "@chakra-ui/react";
+import { NextPage } from 'next'
+
+// Import
+import { gql } from '@apollo/client/core';
+import { useQuery } from '@apollo/client';
+import Storie from '@/components/section_profile';
+
+
+
+
+interface Props { }
+
+const Index: NextPage<Props> = ({ }) => {
+
+//   const POST = gql`
+//     query GetPosts {
+//         getPosts {
+//             id
+//             body
+//             createdAt
+//           user {
+//                 username
+//                 email
+//             }
+//         }
+//     }
+// `;
+  
+  
+//   const { data, loading, error } = useQuery(POST, { onCompleted(data) {
+//       console.log('-->',data);
+//   }, onError(error) {
+//       console.error(error);
+      
+//   },});
+
+  
+  // const { colorMode,   toggleColorMode } = useColorMode();
+  return <div>
+    <Flex flexDirection={"column"}>
+      {/* <Story /> */}
+      <Storie/>
+    </Flex>
+  </div>
+}
+
+export default Index
